291fac84d701a48c00d1b85f589b2ae2a6b8785a
Brendon Co <brendonco@hotmail.com>
2021-03-23 22:53:43 +0800

    Added more test cases

--- todo.js
+++ todo.js
@@ -1,4 +1,12 @@
 const input = `Test Case:
 // TODO Go to work
 // TODO do cooking
+let a = 'TODO';
+let b = 'TODO today';
+let c = "TODO tomorrow";
+let d = "TODO walk with dogs";
+let TODO = 'stuff';
+let e = TODO.length;
+let f = "More work " + TODO + " for today";
+let g = 'bored nothing to do';
 `;
